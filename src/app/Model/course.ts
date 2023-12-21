f3b181e3db6dfcc77a343a59af43eef86a2bd17c
KillerLoM <nguyenminhtam01632.fetel@gmail.com>
2023-12-22 00:55:38 +0700

    init get course by category and edit the css for footer

---
+++ src/app/Model/course.ts
@@ -0,0 +1,11 @@
+export interface Course {
+    id: number,
+    title: string,
+    subtitle: string,
+    author: string,
+    category: string,
+    rating: number,
+    thumbUrl: string,
+    price: number,
+    isFeatured: boolean,
+}
